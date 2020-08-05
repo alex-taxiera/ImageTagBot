b12b484752485a16f79c360fdbd99950bbe02750
Alex Taxiera <alex.taxiera@onesky.com>
2020-08-04 21:30:41 -0400

    restore old migration

---
+++ migrations/20200128232908_old-data.js
@@ -0,0 +1,516 @@
+const data = [
+  {
+    id: 'absolutelyhalal',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/3NZSpz4.jpg',
+    count: 3
+  },
+  {
+    id: 'angery',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/cf6YolH.gif',
+    count: 11
+  },
+  {
+    id: 'arrivederci',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/7nXhqoP.png',
+    count: 0
+  },
+  {
+    id: 'astoniced',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/sLfAIV8.png',
+    count: 2
+  },
+  {
+    id: 'beatyoutodeath',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/VvC3fVV.gif',
+    count: 0
+  },
+  {
+    id: 'BIRDUP',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/hHCl30A.gif',
+    count: 1
+  },
+  {
+    id: 'block',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/4TH72nO.gif',
+    count: 2
+  },
+  {
+    id: 'Bone',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/PQHbHe4.png',
+    count: 1
+  },
+  {
+    id: 'born',
+    user: '154221611166334976',
+    src: 'https://i.imgur.com/aCNYBVw.jpg',
+    count: 1
+  },
+  {
+    id: 'bruh',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/n3m6mvH.gif',
+    count: 1
+  },
+  {
+    id: 'cease',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/tuRfUr9.png',
+    count: 1
+  },
+  {
+    id: 'chacha',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/bN4bPb2.jpg',
+    count: 2
+  },
+  {
+    id: 'cheers',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/6NzsZnE.gif',
+    count: 1
+  },
+  {
+    id: 'chungus',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/yVhNAcG.jpg',
+    count: 3
+  },
+  {
+    id: 'come',
+    user: '154221611166334976',
+    src: 'https://i.imgur.com/sxQw0Oa.jpg',
+    count: 9
+  },
+  {
+    id: 'decide',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/E7PpP1H.png',
+    count: 3
+  },
+  {
+    id: 'disgusting',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/AfYMxpV.png',
+    count: 7
+  },
+  {
+    id: 'doubt',
+    user: '154221611166334976',
+    src: 'https://i.imgur.com/G8DK7ef.png',
+    count: 16
+  },
+  {
+    id: 'ethniccleanse',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/cd67mom.png',
+    count: 2
+  },
+  {
+    id: 'extrathicc',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/pRHpITY.gif',
+    count: 1
+  },
+  {
+    id: 'frown_boi',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/Yo6tIcZ.png',
+    count: 0
+  },
+  {
+    id: 'fuckyouimgay',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/Qz3Ucti.jpg',
+    count: 2
+  },
+  {
+    id: 'give_me',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/SQqD9Wf.gif',
+    count: 3
+  },
+  {
+    id: 'groomer',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/IgH9OmE.jpg',
+    count: 2
+  },
+  {
+    id: 'grump',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/WEcRLFO.png',
+    count: 8
+  },
+  {
+    id: 'gz',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/EUkVxCd.gif',
+    count: 9
+  },
+  {
+    id: 'haha',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/zugfcux.gif',
+    count: 2
+  },
+  {
+    id: 'halt',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/TJ891kW.png',
+    count: 4
+  },
+  {
+    id: 'HappyBirthdayKevin',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/giqatgP.jpg',
+    count: 1
+  },
+  {
+    id: 'hehe',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/v2Jms7d.gif',
+    count: 1
+  },
+  {
+    id: 'help',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/cnVAjaH.jpg',
+    count: 2
+  },
+  {
+    id: 'hmm',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/pvVjwkO.png',
+    count: 5
+  },
+  {
+    id: 'how_caals_girl_do_him',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/1oGSspb.gif',
+    count: 3
+  },
+  {
+    id: 'i',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/QZ9nOxq.png',
+    count: 3
+  },
+  {
+    id: 'in-da-box',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/m7ts3Uh.jpg',
+    count: 1
+  },
+  {
+    id: 'isee',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/piCfsNd.png',
+    count: 0
+  },
+  {
+    id: 'itsretarded',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/d1a91iR.png',
+    count: 0
+  },
+  {
+    id: 'L',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/0evyDR2.png',
+    count: 3
+  },
+  {
+    id: 'ledo',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/IXUeKAg.gif',
+    count: 0
+  },
+  {
+    id: 'logan',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/BAovBLC.png',
+    count: 1
+  },
+  {
+    id: 'luigi',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/LEXW5ri.png',
+    count: 6
+  },
+  {
+    id: 'mafia',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/17e4WBw.png',
+    count: 1
+  },
+  {
+    id: 'manning',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/xaRldwu.jpg',
+    count: 0
+  },
+  {
+    id: 'mcfuckingkillyourself',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/wOjZD3a.png',
+    count: 0
+  },
+  {
+    id: 'mike',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/JzEAmoF.png',
+    count: 2
+  },
+  {
+    id: 'monster',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/kt5FvRL.png',
+    count: 2
+  },
+  {
+    id: 'N',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/tHoXiMZ.png',
+    count: 101
+  },
+  {
+    id: 'nitpicking',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/V3mgvQQ.gif',
+    count: 2
+  },
+  {
+    id: 'nodignity',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/oELzo7F.png',
+    count: 3
+  },
+  {
+    id: 'nut',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/jou3Wlw.png',
+    count: 0
+  },
+  {
+    id: 'o',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/40kXHyt.png',
+    count: 2
+  },
+  {
+    id: 'objection',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/ydIm4bZ.png',
+    count: 1
+  },
+  {
+    id: 'OhBoy',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/08WISl2.png',
+    count: 13
+  },
+  {
+    id: 'partyparrot',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/P9sMEbN.gif',
+    count: 0
+  },
+  {
+    id: 'pathetic',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/uRU6Vcb.png',
+    count: 6
+  },
+  {
+    id: 'pinkwojak',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/oKZKSIw.png',
+    count: 1
+  },
+  {
+    id: 'playitcool',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/baa6uf0.png',
+    count: 0
+  },
+  {
+    id: 'please',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/NqVovQX.gif',
+    count: 4
+  },
+  {
+    id: 'poke-design-shit',
+    user: '229651386223034368',
+    src: 'https://i.imgur.com/q7HgwJJ.png',
+    count: 1
+  },
+  {
+    id: 'professionalretard',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/KPKrkWX.png',
+    count: 2
+  },
+  {
+    id: 'reported',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/RG4Gj7r.png',
+    count: 28
+  },
+  {
+    id: 'rooddood',
+    user: '220606145759412224',
+    src: 'https://i.imgur.com/TmI0Gq2.jpg',
+    count: 9
+  },
+  {
+    id: 'samus',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/o0cMtg7.png',
+    count: 0
+  },
+  {
+    id: 'screm',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/VWJBktR.jpg',
+    count: 0
+  },
+  {
+    id: 'ShutUpAndrew',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/4zejTrW.png',
+    count: 43
+  },
+  {
+    id: 'sike',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/AXwx9pN.jpg',
+    count: 2
+  },
+  {
+    id: 'spaceguy',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/YfCJf4q.png',
+    count: 0
+  },
+  {
+    id: 'squad',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/4U4aNa9.gif',
+    count: 1
+  },
+  {
+    id: 'stop',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/AUMA8jS.png',
+    count: 2
+  },
+  {
+    id: 'thinkspin',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/Vw0yw8h.gif',
+    count: 2
+  },
+  {
+    id: 'thonk',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/IjtTNx1.png',
+    count: 4
+  },
+  {
+    id: 'torture',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/Szx59p7.gif',
+    count: 1
+  },
+  {
+    id: 'ubully',
+    user: '192768112209297409',
+    src: 'https://i.imgur.com/nVdahlu.jpg',
+    count: 1
+  },
+  {
+    id: 'ultra_bruh',
+    user: '192158164798406658',
+    src: 'https://i.imgur.com/MjprzlY.jpg',
+    count: 1
+  },
+  {
+    id: 'vaultboy',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/oDd2wSc.png',
+    count: 0
+  },
+  {
+    id: 'wack',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/vAtIbZh.png',
+    count: 1
+  },
+  {
+    id: 'whynotboth',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/8gOllyY.gif',
+    count: 4
+  },
+  {
+    id: 'Wow',
+    user: '192768112209297409',
+    src: 'https://i.imgur.com/EpAbiVP.jpg',
+    count: 19
+  },
+  {
+    id: 'woweegames',
+    user: '220606145759412224',
+    src: 'https://i.imgur.com/LeXFo0U.jpg',
+    count: 22
+  },
+  {
+    id: 'yes',
+    user: '434507972379672587',
+    src: 'https://i.imgur.com/P8o09Xe.gif',
+    count: 32
+  },
+  {
+    id: 'yike',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/m1Rt7do.png',
+    count: 5
+  },
+  {
+    id: 'yikes',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/FvgdyoE.jpg',
+    count: 27
+  },
+  {
+    id: 'yikeshardlemonade',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/WsQovSp.jpg',
+    count: 8
+  },
+  {
+    id: 'yoangelo',
+    user: '340308249871908864',
+    src: 'https://i.imgur.com/jXw5EJN.jpg',
+    count: 1
+  }
+]
+
+exports.up = (knex) => {
+  return knex('tag').insert(data)
+}
+
+exports.down = (knex) => {
+  return knex('tag')
+    .whereIn('id', data.map((d) => d.id))
+    .del()
+}
