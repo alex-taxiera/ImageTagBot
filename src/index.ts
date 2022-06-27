16e2297e23ce6b9990167ff8814d27437a298da3
Alex Taxiera <alex.taxiera@gmail.com>
2022-06-27 16:36:46 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
 import {
-  join
-} from 'path'
-import {
-  SQLManager
-} from 'eris-boiler'
+  Event,
+  Forge,
+  TopLevelCommand,
+} from '@hephaestus/eris'
 
 import { ENV } from './types/env'
-import {
-  TaggerClient
-} from '@tagger'
-
-import {
-  oratorOptions,
-  statusManagerOptions
-} from './config'
-import { logger } from 'eris-boiler/util'
 
 main().catch(() => undefined)
 
-async function main () {
+const exampleCommand: TopLevelCommand = {
+  type: 1,
+  guildId: '436591833196265473',
+  name: 'example',
+  description: 'example description',
+  action: (interaction, client) => {
+    void interaction.createMessage('example message')
+  },
+}
+
+const readyEvent: Event = {
+  name: 'ready',
+  handler: () => console.log('ready'),
+}
+
+async function main (): Promise<void> {
   if (process.env.NODE_ENV === 'production') {
     await import('docker-secret-env').then(({ load }) => load())
   } else {
@@ -27,30 +32,32 @@ async function main () {
 
   const {
     DISCORD_TOKEN,
-    IMGUR_CLIENT_ID,
-    DB_CLIENT,
-    DB_NAME,
-    DB_USER,
-    DB_PASS,
-    DB_HOST
+    // IMGUR_CLIENT_ID,
+    // DB_CLIENT,
+    // DB_NAME,
+    // DB_USER,
+    // DB_PASS,
+    // DB_HOST,
   } = (process.env as unknown) as ENV
 
-  const bot = new TaggerClient(DISCORD_TOKEN, IMGUR_CLIENT_ID, {
-    oratorOptions,
-    statusManagerOptions,
-    databaseManager: new SQLManager({
-      connectionInfo: {
-        database: DB_NAME,
-        user: DB_USER,
-        password: DB_PASS,
-        host: DB_HOST
-      },
-      client: DB_CLIENT
-    })
-  })
+  // const bot = new TaggerClient(DISCORD_TOKEN, IMGUR_CLIENT_ID, {
+  //   oratorOptions,
+  //   statusManagerOptions,
+  //   databaseManager: new SQLManager({
+  //     connectionInfo: {
+  //       database: DB_NAME,
+  //       user: DB_USER,
+  //       password: DB_PASS,
+  //       host: DB_HOST
+  //     },
+  //     client: DB_CLIENT
+  //   })
+  // })
 
-  bot
-    .addCommands(join(__dirname, 'commands'))
-    .connect()
-    .catch(logger.error)
+  const client = new Forge(DISCORD_TOKEN)
+
+  // client.commands.add(join(__dirname, 'commands'))
+  client.events.add([ readyEvent ])
+  client.commands.add([ exampleCommand ])
+  client.connect().catch(console.error)
 }
